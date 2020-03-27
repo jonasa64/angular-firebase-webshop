3c75a46c7dad83b76416174eb193f70dad467dd1
jonasa64 <hvidovrefifa@gmail.com>
2020-03-27 23:24:27 +0100

    rendered products

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {map} from 'rxjs/operators';
+import {Product} from '../../product.model';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+productCol: AngularFirestoreCollection<Product>;
+products: any;
 
-  constructor() { }
+  constructor(private  afs: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.productCol = this.afs.collection('products');
+    this.products = this.productCol.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Product;
+          const  id = a.payload.doc.id;
+          return {data, id};
+        });
+      } )
+    );
   }
 
 }
